c01a85fc8d66af977b1614b4ca72551c4980e9e5
Ashmit Raj <ashmitraj089@gmail.com>
2021-02-13 15:06:35 +0530

    second commit

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -11,9 +11,7 @@ function Slider(){
                 <div className="carousel-item">
                 <img style={{marginLeft:'35%', width:'25%', height:'50vh',boxShadow:'0px 8px 15px rgb(0,0,0,0.9)'}} src="https://yt3.ggpht.com/a/AGF-l7-_b35EjMh3sffVQFyqHxAPj_O9fVJuoOmwMg=s288-c-k-c0xffffffff-no-rj-mo" alt="Second slide"/>
                 </div>
-                <div className="carousel-item">
-                <img style={{marginLeft:'35%', width:'25%', height:'50vh',boxShadow:'0px 8px 15px rgb(0,0,0,0.9)'}} src="https://media-exp1.licdn.com/dms/image/C5603AQEZbvEphMR8AQ/profile-displayphoto-shrink_200_200/0/1609676083154?e=1615420800&v=beta&t=C5_VCZfokmLfdslDoOKgWfVmOQJi4JrzLaPf9sSNiso" alt="Third slide"/>
-                </div>
+                
             </div>
             <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
